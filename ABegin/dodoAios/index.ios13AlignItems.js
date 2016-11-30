623b28446f4c3d06b5cee25f39a5dce0d8c50b1d
dodo_lihao <2390183798@qq.com>
2016-11-30 12:18:51 +0800

    commit
    Mac上面，一直不显示 .gitignore文件
    哎，还好客户端偶尔显示， 填上build等文件夹的忽略才敢提交
    不然，又是个悲剧

---
+++ ABegin/dodoAios/index.ios13AlignItems.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native';
+
+class ABegin extends Component {
+  render() {
+    return (
+      // 尝试把`alignItems`改为`flex-start`看看
+      // 尝试把`justifyContent`改为`flex-end`看看
+      // 尝试把`flexDirection`改为`row`看看
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+};
+
+AppRegistry.registerComponent('ABegin', () => ABegin);
